13dbda3725ea1b324b22f67a186a8b0d9c862425
Johan Castillo <jcjohan2707@gmail.com>
2021-07-08 16:14:07 -0400

    Animation to navbar

--- cba-frontend/src/app/components/navigation/navigation.component.ts
+++ cba-frontend/src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  public scroll = 0;
+  public renderer2: Renderer2;
+
+
+  // Elements html
+
+  constructor(renderer2: Renderer2) {
+
+  }
+
+  @ViewChild("asNavigation") public navigation: ElementRef;
 
   ngOnInit(): void {
+    // Navigation animate
+    addEventListener('scroll', () => {
+        let navEl = this.navigation.nativeElement
+        this.scroll = window.scrollY;
+
+        if(this.scroll > 110){
+          navEl.classList.add('activate')
+        }else{
+          navEl.classList.remove('activate')
+        }
+
+    })
+
   }
 
 }
